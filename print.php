01befac0f87df723f34af73503922226c9e17547
Kirill Maftulyak <kirillmaftulyak@Kirills-MacBook-Pro.local>
2019-09-23 11:39:10 +0300

    PHP - Teksi väljastus print ja echo abil

---
+++ print.php
@@ -0,0 +1,11 @@
+<?php
+print "See on esimene php rida<br>";
+print "See on teise php rida<br>";
+print "See on kolmanda php rida<br>";
+/*
+ * See on
+ * mitmerealine
+ * kommentaar
+ * */
+// echo abil printimine
+echo "See on tekst, mis kirjutatud echo abil";
