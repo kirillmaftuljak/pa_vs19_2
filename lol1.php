59529a8fbe944be11a9d849af5e4843d64153028
Kirill Maftulyak <kirillmaftulyak@Kirills-MacBook-Pro.local>
2019-09-23 12:11:55 +0300

    PHP - yikes

---
+++ lol1.php
@@ -0,0 +1,5 @@
+<?php>
+echo '<h1>Info minu kohta</h1>';
+echo '<p>Kirill Maftuljak</p>';
+echo '<p>IKT OSAKOND</p>';
+echo '<p><a href="mailto:kirill.maftuljak@khk.ee">kirill.maftuljak@khk.ee</a></p>';
