c4994e229b64d4c7a9267a60c719fe53dcfca9a7
Kirill Maftulyak <kirillmaftulyak@Kirills-MacBook-Pro.local>
2019-09-23 12:25:42 +0300

    PHP - kkk

---
+++ kkk.php
@@ -0,0 +1,21 @@
+<?php>
+// Muutajate defineerimine = väärtuse omistamine
+// $muutujaNimi = väärtus;
+//
+//  Andmetüübid
+//  Sõned - String
+//  "teksti kujulised andmed"
+// "või niisuguste jutumärkide vahel ka"
+//  Numbrid - täisarvud - 1,2,175, -25, ...
+//  Numbrid - reaalarvud - 1.5, -2.89, ...
+//
+
+$nimi = "Kirill Maftuljak";
+$perenimi = "Maftuljak"
+$yksus = "IKT OSAKOND";
+$email = "kirill.maftuljak@khk.ee";
+
+// muutujate kasutamine - kutsumine
+echo $nimi."<br>";
+echo $yksus."<br>";
+echo $email."<br>";
